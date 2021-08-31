5eebfb28a8356a7c579347e5ed1d88dd9bd6a2e3
Alan Fernandes <alanff3878@outlook.com>
2021-08-31 19:28:08 -0300

    arrumado

---
+++ index.js
@@ -0,0 +1,11 @@
+let itens = ["arroz", "feijão", "farofa", "batata", "carne"];
+console.log(itens);
+
+itens.unshift("queijo");
+itens.pop();
+itens.push("vinagrete", "maionese");
+itens.shift();
+
+const numeros = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+numeros.sort();
+console.log(numeros);
